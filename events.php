86938aee2b0ae9b8ae4e492a729b69d089bbf8cb
danilsizov1 <danilsizov3@gmail.com>
2017-02-20 16:13:40 +0200

    finished event.php
    start create about-us

---
+++ events.php
@@ -0,0 +1,204 @@
+
+<?php
+/*
+Template Name: events
+*/
+?>
+
+<?php
+/**
+ * Шаблон обычной страницы (page.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+get_header(); // подключаем header.php ?>
+<section>
+	<div class="blue-background"></div>
+	<div class="gold-background"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="vebinar-tittle-small">Мероприятие</div>
+				<div class="vebinar-tittle">
+					Конференции <br>и встречи клуба
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="telephone-black">099 547-89-45</div>
+				<div class="invite">Заявка на участие</div>
+				<input type="text" class="input mar-top-input" placeholder="Введите E-mail">
+				<input type="text" class="input" placeholder="Введите E-mail">
+				<input type="text" class="input" placeholder="Введите E-mail">
+				<div class="invite-buton">Подписаться</div>
+			</div>
+		</div>
+		<div class="container content">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="block-tittle">
+						Встречи<br>
+						клуба друзей<br>
+						Перформия
+					</div>
+				</div>
+				<div class="col-lg-1"></div>
+				<div class="col-lg-7 quote">
+					<div class="nav-line"></div>
+					<div class="quote-text">
+						<p>Участники Клуба Друзей Перформии - это люди, которые прошли у нас обучение, являются нашими единомышленниками и разделяют наши ценности. Вы можете стать участником Клуба Друзей Перформии, если Вы применяете нашу технологию найма, считаете, что Перформия приносит практическую пользу бизнес сообществу, и рекомендуете нас как эксперта в области найма своим знакомым и деловым партнёрам. </p>
+						<p><img src="<?php echo get_template_directory_uri();?>/img/photo-event.png" ></p>
+					</div>
+					<p>Мы регулярно организовываем встречи  Друзей Перформии, на которых делимся своими открытиями и полезными рекомендациями в области найма и управления персоналом.  Нашими друзьями являются представители бизнес-сообщества: руководители, предприниматели и собственники бизнеса.</p>
+
+					<p>Мы создали систему вознаграждения. Если другие компании начинают сотрудничать с нами по Вашим рекомендациям, мы начисляем Вам баллы. Накопив определённое количество баллов, Вы можете посетить любой из наших семинаров бесплатно.</p>
+					<div class="nearest-event">
+						<div class="nearest-micro-tittle">Ближайшая встреча</div>
+						<div class="nearest-tittle">Проблемы найма</div>
+						<div class="nearest-text">Обсудим проблемы внедрения и поделимся опытом решения. 
+						После сыграем в Мафию.</div>
+						<div class="link-to-FB">Подробнее в фейсбуке ></div>
+						<div class="row">
+						<div class="col-lg-6">
+							<div class="about-tittle">300</div>
+							<div class="description">Гривен стоит</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="about-tittle">18 сен</div>
+							<div class="description">С 10 до 14</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="about-tittle">Ивано-Франковск</div>
+							<div class="description">Город</div>
+						</div>
+						<div class="gray-line"></div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container content">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="block-tittle"><strong>Конгресс</strong></div>
+				</div>
+				<div class="col-lg-1"></div>
+				<div class="col-lg-7">
+					<div class="main-programm">
+						<div class="content-main-programm">
+							<div class="main-programm-tittle">
+								«Высший пилотаж — 
+								индивидуальное
+								вовлечение»
+							</div>
+							<div class="main-programm-about">
+								Подробнее о технологии >
+							</div>
+							<div class="main-programm-card">
+								<div class="main-programm-line"></div>
+								<div class="main-programm-seminar-place">
+									<div class="seminar-geo"><img src="<?php echo get_template_directory_uri();?>/img/green-circle.png"><strong> Одесса</strong></div>
+									<div class="main-programm-seminar-time">с 17.08 до 19.08</div>
+								</div>
+								<div class="main-programm-seminar-place">
+									<div class="seminar-geo"><img src="<?php echo get_template_directory_uri();?>/img/green-circle.png"><strong> 9 сентября</strong></div>
+									<div class="main-programm-seminar-time">с 10 до 19</div>
+								</div>
+								<div class="main-programm-seminar-price"><img src="<?php echo get_template_directory_uri();?>/img/green-circle.png"><strong> 14 000 грн. </strong></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	<div class="container content">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="block-tittle">
+						Ежегодная конференция
+					</div>
+				</div>
+				<div class="col-lg-1"></div>
+				<div class="col-lg-7 quote">
+					<div class="nav-line"></div>
+					<div class="quote-text">
+						<p>Раз в год мы собираем наших друзей, наших выпускников на ГЛАВНОЕ СОБЫТИЕ – КОНФЕРЕНЦИЮ.Каждый год это новые места, новые темы и встреча старых ДРУЗЕЙ! Семь дней всегда пролетают на одном дыхании.</p>
+
+						<p>Мы приглашаем приезжать наших клиентов семьями. Каждому члену семьи будет и интересно, и очень полезно. Для каждого будет свое занятие!
+						Конференции проводятся по темам. Все семь дней идут на одну тему. В 2016 году была тема — PR.</p>
+
+						<p>Весь материал практически для всех участников совершенно новый. Не просто теория, а проверенные практические инструменты в какой-то из областей.
+						Конференции «ПЕРФОРМИИ» – это уникальная возможность познакомиться и наладить деловые контакты с бизнесменами из России, Казахстана, Молдавии, Белоруссии, Украины и Латвии.</p>
+						<p><img src="<?php echo get_template_directory_uri();?>/img/confirention.png" class="minus-left-mar"></p>
+					</div>
+					<h3>Фото и видео с прошлых конференций</h3>
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="card-download">
+								<img src="<?php echo get_template_directory_uri();?>/img/batumi.png" class="minus-left-mar">
+								<div class="locate"><strong>Батуми, 2016</strong></div>
+								<div class="download-button"><img src="<?php echo get_template_directory_uri();?>/img/download.png"></div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card-download">
+								<img src="<?php echo get_template_directory_uri();?>/img/batumi.png" class="minus-left-mar">
+								<div class="locate"><strong>Батуми, 2016</strong></div>
+								<div class="download-button"><img src="<?php echo get_template_directory_uri();?>/img/download.png"></div>
+							</div>
+						</div>
+					</div>
+					<div class="row card-mar-top">
+						<div class="col-lg-6">
+							<div class="card-download">
+								<img src="<?php echo get_template_directory_uri();?>/img/batumi.png" class="minus-left-mar">
+								<div class="locate"><strong>Батуми, 2016</strong></div>
+								<div class="download-button"><img src="<?php echo get_template_directory_uri();?>/img/download.png"></div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card-download">
+								<img src="<?php echo get_template_directory_uri();?>/img/batumi.png" class="minus-left-mar">
+								<div class="locate"><strong>Батуми, 2016</strong></div>
+								<div class="download-button"><img src="<?php echo get_template_directory_uri();?>/img/download.png"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+	</div>
+	<div class="gold-block">
+		<div class="container">
+			<div class="row">
+				<div class="gold-tittle">Заявка на участие</div>
+			</div>
+			<div class="row">
+			<div class="col-lg-4"></div>
+				<div class="col-lg-2">
+					<input type="text" class="input mar-top-input" placeholder="Введите E-mail">
+				</div>
+			</div>
+			<div class="row">
+			<div class="col-lg-4"></div>
+				<div class="col-lg-2">
+					<input type="text" class="input" placeholder="Введите E-mail">
+				</div>
+			</div>
+			<div class="row">
+			<div class="col-lg-4"></div>
+				<div class="col-lg-2">
+					<input type="text" class="input" placeholder="Введите E-mail">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4"></div>
+				<div class="col-lg-2">
+					<div class="invite-buton">Подписаться</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</section>
+<?php get_footer(); // подключаем footer.php ?>
