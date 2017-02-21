df697f56297e093c7287e4362f8a22effb038186
danilsizov1 <danilsizov3@gmail.com>
2017-02-21 17:34:19 +0200

    start create responce

--- functions.php
+++ functions.php
@@ -114,6 +114,7 @@ if (!function_exists('add_styles')) { // если ф-я уже есть в до�
 		wp_enqueue_style( 'aboutstyle', get_template_directory_uri().'/css/about-style.css' );
 		wp_enqueue_style( 'tecstyle', get_template_directory_uri().'/css/technology-style.css' );
 		wp_enqueue_style( 'toolstyle', get_template_directory_uri().'/css/tools-style.css' );
+		wp_enqueue_style( 'resp', get_template_directory_uri().'/css/responce.css' );
 	}
 }
 
